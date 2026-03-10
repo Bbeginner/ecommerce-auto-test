95e46f7e5f97684513904dac9678c9be97e6d2f5
Bbeginner <784245232@qq.com>
2026-03-10 23:37:53 +0800

    123

--- tests/test_login.py
+++ tests/test_login.py
@@ -26,7 +26,7 @@ class TestLogin:
         ("locked_out_user", "secret_sauce", "locked"),
         ("fake_user", "wrong_pwd", "invalid"),
         # pytest.param("performance_glitch_user", "secret_sauce", "success", marks=pytest.mark.skip(reason="CI环境偶发不稳定，本地通过")),
-        ("problem_user", "secret_sauce", "success"),
+        # ("problem_user", "secret_sauce", "success"),
         #  pytest.param("problem_user", "secret_sauce", "success", marks=pytest.mark.skip(reason="CI环境偶发不稳定，本地通过")),
         # ("standard_user", "secret_sauce", "success"),  # 故意传参错误的用例放最后
     ])
