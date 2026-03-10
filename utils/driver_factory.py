e66facc1c6d5c136de7412ef5dcf8a058bfebe3d
Bbeginner <784245232@qq.com>
2026-03-10 15:15:11 +0800

    1

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -37,7 +37,7 @@ class DriverFactory:
             log_path = "./chromedriver.log"
 
             if chromedriver_path and os.path.exists(chromedriver_path):
-                service = Service(os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver'))
+                service = ChromeService(os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver'))
                 driver = webdriver.Chrome(service=service, options=options)
                 # service = ChromeService(
                 #     executable_path=chromedriver_path,
